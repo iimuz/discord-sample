dbfa9894cb9ff2b1bb729ccc67688a483a5cdf7f
iimuz <iz.primary.story+github@gmail.com>
2023-12-10 17:43:17 +0900

    fix: 公式サンプルの形状に変更

--- src/til-20231210/src/dm-response.py
+++ src/til-20231210/src/dm-response.py
@@ -1,27 +1,32 @@
+"""DiscordでDMに対して返答するスクリプト.
+
+利用例: `python src/dm-response.py`
+"""
 import os
 
 import discord
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
 
+class DiscordClient(discord.Client):
+    """DiscordのDMに対して反応するためのクライアント."""
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+    async def on_ready(self):
+        """準備完了時の動作."""
+        print("Logged on as", self.user)
 
+    async def on_message(self, message):
+        """メッセージ取得時の動作."""
+        if message.author == self.user:
+            return
 
-@client.event
-async def on_message(message):
-    print(message)
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+        if message.content == "ping":
+            await message.channel.send("pong")
 
 
 if __name__ == "__main__":
     TOKEN = os.environ.get("BOT_TOKEN", "")
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = DiscordClient(intents=intents)
     client.run(TOKEN)
